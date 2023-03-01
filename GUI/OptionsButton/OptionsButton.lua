aa37ee2a0a457210cff4ac4e55b3f0369470826d
Krowi <Krowi@Home>
2023-03-01 15:09:11 +0100

    changed saved variables, made rows/columns editable for merchant info

--- GUI/OptionsButton/OptionsButton.lua
+++ GUI/OptionsButton/OptionsButton.lua
@@ -1,22 +1,82 @@
--- hooksecurefunc("MerchantFrame_Update", function()
--- 	if (MerchantFrame.selectedTab == 1) then
--- 		-- Addon:UpdateExtensionPanel();
--- 		MerchantFrameLootFilter:SetPoint("TOPRIGHT", MerchantFrame, "TOPRIGHT", -35, -28);
--- 		VendorerToggleExtensionFrameButtons:Show();
--- 	else
--- 		MerchantFrame_UpdateBuybackInfo();
--- 		-- Addon:HideExtensionPanel();
--- 		MerchantFrameLootFilter:SetPoint("TOPRIGHT", MerchantFrame, "TOPRIGHT", 0, -28);
--- 		VendorerToggleExtensionFrameButtons:Hide();
--- 		-- VendorerStackSplitFrame:Cancel();
--- 	end
--- end);
-
 -- [[ Namespaces ]] --
 local _, addon = ...;
 
+local originalWidth, originalHeight = MerchantFrame:GetSize();
+local xFirstOffset, yFirstOffset = 11, -69;
+local xOffset, yOffsetMerchantInfo, yOffsetBuybackInfo = 12, 8, 15;
+local itemWidth, itemHeight = MerchantItem1:GetSize();
+
+local itemSlotTable = {};
+for i = 1, 12, 1 do
+	tinsert(itemSlotTable, _G["MerchantItem" .. i]);
+end
+
+local function ResetTable(t)
+	for _, v in next, t do
+		v:Hide();
+	end
+end
+
+local function ResetItemSlots()
+	ResetTable(itemSlotTable);
+end
+
+local function GetItemSlot(index)
+	if itemSlotTable[index] then
+		return itemSlotTable[index];
+	end
+	local frame = CreateFrame("Frame", "MerchantItem" .. index, MerchantFrame, "MerchantItemTemplate");
+	itemSlotTable[index] = frame;
+	return frame;
+end
+
+local function DrawItemSlots(rows, columns, yOffset)
+	for column = 1, columns, 1 do
+		for row = 1, rows, 1 do
+			local index = (row - 1) * columns + column;
+			local itemSlot = GetItemSlot(index);
+			itemSlot:ClearAllPoints();
+			itemSlot:SetPoint("TOPLEFT", MerchantFrame, "TOPLEFT", xFirstOffset + (column - 1) * (xOffset + itemWidth), yFirstOffset - (row - 1) * (yOffset + itemHeight));
+			itemSlot:Show();
+		end
+	end
+end
+
+local function DrawMerchantInfo()
+	ResetItemSlots();
+
+	local columns = addon.Options.db.Columns;
+	local rows = addon.Options.db.Rows;
+
+	DrawItemSlots(rows, columns, yOffsetMerchantInfo);
+	MerchantBuyBackItem:ClearAllPoints();
+	MerchantBuyBackItem:SetPoint("BOTTOMRIGHT", MerchantFrameInset, "BOTTOMRIGHT", -1, 6);
+	MerchantBuyBackItem:Show();
+end
+
+local function DrawBuybackInfo()
+	ResetItemSlots();
+
+	MerchantFrame:SetSize(originalWidth, originalHeight);
+
+	local columns = 2;
+	local rows = 6;
+
+	DrawItemSlots(rows, columns, yOffsetBuybackInfo);
+	MerchantBuyBackItem:Hide();
+end
+
+hooksecurefunc("MerchantFrame_UpdateMerchantInfo", function()
+	DrawMerchantInfo();
+end);
+
+hooksecurefunc("MerchantFrame_UpdateBuybackInfo", function()
+	DrawBuybackInfo();
+end);
+
 local function UpdateView()
 	print(addon.Options.db.Rows, addon.Options.db.Columns);
+	MerchantFrame_Update();
 	-- Based on these numbers, calculate the number of items that can be shown
 	-- Pull them from the pool or create new ones if needed
 	-- MerchantItemTemplate
--- Options/Options.lua
+++ Options/Options.lua
@@ -4,6 +4,6 @@ addon.Options = {}; -- Will be overwritten in Load (intended)
 local options = addon.Options;
 
 function options.Load()
-    addon.Options = LibStub("AceDB-3.0"):New("Options", options.Defaults, true);
+    addon.Options = LibStub("AceDB-3.0"):New("KrowiV_Options", options.Defaults, true);
     addon.Options.db = addon.Options.profile;
 end
