d6432ab6cb0da36bf363623bae3f1924cd4af2c6
Krowi <Krowi@Home>
2023-03-02 18:11:18 +0100

    Moved next page button + flexible navigation bar show/hide

--- GUI/MerchantFrame/MerchantFrame.lua
+++ GUI/MerchantFrame/MerchantFrame.lua
@@ -14,11 +14,26 @@ hooksecurefunc("MerchantFrame_UpdateMerchantInfo", function()
 
 	local width = originalWidth + numColumns * itemWidth;
 	local height = originalHeight + numRows * itemHeight;
+	if not MerchantPageText:IsShown() then
+		height = height - 36;
+	end
 	MerchantFrame:SetSize(width, height);
+	KrowiV_BottomExtensionRightBorder:Show();
+	if numColumns > 0 then
+		KrowiV_BottomExtensionLeftBorder:Show();
+		KrowiV_BottomExtensionMidBorder:Show();
+	else
+		MerchantFrameBottomRightBorder:Hide();
+		KrowiV_BottomExtensionLeftBorder:Hide();
+		KrowiV_BottomExtensionMidBorder:Hide();
+	end
 end);
 
 hooksecurefunc("MerchantFrame_UpdateBuybackInfo", function()
 	MerchantFrame:SetSize(originalWidth, originalHeight);
+	KrowiV_BottomExtensionRightBorder:Hide();
+	KrowiV_BottomExtensionLeftBorder:Hide();
+	KrowiV_BottomExtensionMidBorder:Hide();
 end);
 
 local function PrepareMerchantFrame()
@@ -38,20 +53,23 @@ end);
 
 MerchantFrameBottomRightBorder:SetTexCoord(91 / 256, (91 + 76) / 256, 0, 0.4765625);
 
-local bottomExtensionRightBorder = MerchantFrame:CreateTexture();
+local bottomExtensionRightBorder = MerchantFrame:CreateTexture("KrowiV_BottomExtensionRightBorder");
 bottomExtensionRightBorder:SetSize(76, 61);
 bottomExtensionRightBorder:SetTexture("Interface/MerchantFrame/UI-Merchant-BottomBorder");
 bottomExtensionRightBorder:SetTexCoord(0, 0.296875, 0.4765625, 0.953125);
 bottomExtensionRightBorder:SetPoint("BOTTOMRIGHT", MerchantFrameInset, "BOTTOMRIGHT", 3, 0);
 
-local bottomExtensionLeftBorder = MerchantFrame:CreateTexture();
+local bottomExtensionLeftBorder = MerchantFrame:CreateTexture("KrowiV_BottomExtensionLeftBorder");
 bottomExtensionLeftBorder:SetSize(89, 61);
 bottomExtensionLeftBorder:SetTexture("Interface/MerchantFrame/UI-Merchant-BottomBorder");
 bottomExtensionLeftBorder:SetTexCoord((91 + 76) / 256, 1, 0, 0.4765625);
 bottomExtensionLeftBorder:SetPoint("TOPLEFT", MerchantFrameBottomRightBorder, "TOPRIGHT", 0, 0);
 
-local bottomExtensionMidBorder = MerchantFrame:CreateTexture();
+local bottomExtensionMidBorder = MerchantFrame:CreateTexture("KrowiV_BottomExtensionMidBorder");
 bottomExtensionMidBorder:SetTexture("Interface/MerchantFrame/UI-Merchant-BottomBorder");
 bottomExtensionMidBorder:SetTexCoord(8 / 256, (8 + 151) / 256, 0, 0.4765625);
 bottomExtensionMidBorder:SetPoint("TOPLEFT", bottomExtensionLeftBorder, "TOPRIGHT", 0, 0);
 bottomExtensionMidBorder:SetPoint("BOTTOMRIGHT", bottomExtensionRightBorder, "BOTTOMLEFT", 0, 0);
+
+MerchantPrevPageButton:SetPoint("BOTTOMLEFT", MerchantFrameBottomLeftBorder, "TOPLEFT", 8, -5);
+MerchantNextPageButton:SetPoint("BOTTOMRIGHT", KrowiV_BottomExtensionRightBorder, "TOPRIGHT", -7, -5);
